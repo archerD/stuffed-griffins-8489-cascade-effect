337d7b316bc18363709153ad1b94e7eeafd3b2a3
unknown <dflores0818@gmail.com>
2014-12-03 12:31:37 -0800

    fixed auto writer program

---
+++ autoWriter test.c
@@ -0,0 +1,70 @@
+TFileHandle readFrom;
+TFileIOResult result;
+
+void errorCheck(TFileIOResult check)
+{
+	if(check != 0)
+	{
+		nxtDisplayCenteredTextLine(4, "error");
+		wait(2);
+	}
+}
+
+byte lastRead()
+{
+	byte N;
+	TFileIOResult result;
+	ReadByte(readFrom, result, N);
+	return N;
+	errorCheck(result);
+}
+
+task main()
+{
+	word currentSize;
+	OpenRead(readFrom, result, "auto.txt", currentSize);
+	errorCheck(result);
+
+	byte read = lastRead();
+	if(read == 1)
+	{
+		nxtDisplayCenteredTextLine(3, "ramp");
+		wait(2);
+		eraseDisplay();
+	}
+	else if(read == 2)
+	{
+		nxtDisplayCenteredTextLine(3, "parking zone");
+		wait(2);
+		eraseDisplay();
+	}
+
+  read = lastRead();
+
+  if(read == 1)
+  {
+		nxtDisplayCenteredTextLine(3, "ball tube");
+		wait(2);
+		eraseDisplay();
+	}
+
+	read = lastRead();
+
+	if(read == 1)
+	{
+		nxtDisplayCenteredTextLine(3, "center goal");
+		wait(2);
+		eraseDisplay();
+	}
+
+	read = lastRead();
+
+	if(read == 1)
+	{
+		nxtDisplayCenteredTextLine(3, "kickstand");
+		wait(2);
+		eraseDisplay();
+	}
+
+	Close(readFrom, result);
+}
